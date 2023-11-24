c41862a130daf3bfe03452231b218133dee31056
Antohi Robert <antohir@gmail.com>
2023-11-24 11:36:34 +0200

    Partial

--- Partial/ex1.py
+++ Partial/ex1.py
@@ -1,4 +1,9 @@
 import random
+from pgmpy.models import BayesianNetwork
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
+import matplotlib.pyplot as plt
+import networkx as nx
 
 firstplayerwin=0
 secondplayerwin=0
@@ -62,4 +67,24 @@ print("Second player win " +str(secondplayerwin))
 
 #First player win 3883
 #Second player win 6117
+
+#Punctul 2
+
+# Defining the model structure. We can define the network by just passing a list of edges.
+model = BayesianNetwork([('First_throw', 'J0_first_round'), ('First_throw', 'J1_first_round'), ('J0_first_round')])
+
+# Defining individual CPDs.
+cpd_FT = TabularCPD(variable='First_throw', variable_card=2, values=[[0.5], [0.5]]) #First throw
+cpd_J0_first_round = TabularCPD(variable='J0_first_round', variable_card=2, values=[[0.5], [0.5]]) # First player toss in first round
+cpd_J1_first_round = TabularCPD(variable='J1_first_round', variable_card=2, values=[[0.66], [0.33]]) # Second player toss in first round
+
+# The CPD for C is defined using the conditional probabilities based on U and R
+cpd_c = TabularCPD(variable='C', variable_card=2, 
+                   values=[[0.8, 0.5, 0.2, 0.0], 
+                           [0.2, 0.5, 0.8, 1.0]],
+                  evidence=['U', 'R'],
+                  evidence_card=[2, 2])
+
+# Associating the CPDs with the network
+model.add_cpds(cpd_r, cpd_u, cpd_c)
 
