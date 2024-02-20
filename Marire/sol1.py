0b749a3ff123ff19776f23d422e5016116eb74e4
Antohi Robert <antohir@gmail.com>
2024-02-20 11:21:34 +0200

    Mica corectura

--- Marire/sol1.py
+++ Marire/sol1.py
@@ -7,36 +7,36 @@ from pgmpy.models import BayesianModel
 from pgmpy.factors.discrete import TabularCPD
 
 '''
-robertantohi@Roberts-MacBook-Pro Marire % poetry run python sol1.py
+robertantohi@Roberts-MacBook-Pro Marire % poetry run python sol1.py 
 Probabilitatea ca maine sa ploua: +------------+---------------+
 | Ploaie     |   phi(Ploaie) |
 +============+===============+
-| Ploaie(Da) |        0.4690 |
+| Ploaie(Nu) |        0.4690 |
 +------------+---------------+
-| Ploaie(Nu) |        0.5310 |
+| Ploaie(Da) |        0.5310 |
 +------------+---------------+
 Probabilitatea ca maine sa ploua, avand in vedere ca vremea este innorata: +------------+---------------+
 | Ploaie     |   phi(Ploaie) |
 +============+===============+
-| Ploaie(Da) |        0.4000 |
+| Ploaie(Nu) |        0.4000 |
 +------------+---------------+
-| Ploaie(Nu) |        0.6000 |
+| Ploaie(Da) |        0.6000 |
 +------------+---------------+
 Probabilitatea ca maine sa ploua, avand in vedere ca precizia citirii barometrului este scazuta: +------------+---------------+
 | Ploaie     |   phi(Ploaie) |
 +============+===============+
-| Ploaie(Da) |        0.4158 |
+| Ploaie(Nu) |        0.4158 |
 +------------+---------------+
-| Ploaie(Nu) |        0.5842 |
+| Ploaie(Da) |        0.5842 |
 +------------+---------------+
 Probabilitatea ca astazi sa fie o zi innorata, avand in vedere ca va ploua maine: +-----------------------+----------------------+
 | Starea Vremii         |   phi(Starea Vremii) |
 +=======================+======================+
-| Starea Vremii(Soare)  |               0.5181 |
+| Starea Vremii(Soare)  |               0.1073 |
 +-----------------------+----------------------+
-| Starea Vremii(Noros)  |               0.4264 |
+| Starea Vremii(Noros)  |               0.5650 |
 +-----------------------+----------------------+
-| Starea Vremii(Ploios) |               0.0554 |
+| Starea Vremii(Ploios) |               0.3277 |
 +-----------------------+----------------------+
 '''
 
@@ -64,9 +64,9 @@ cpd_precizie = TabularCPD(variable='Precizia Citirii Barometrului', variable_car
 cpd_ploaie = TabularCPD(variable='Ploaie', variable_card=2, 
                         evidence=['Starea Vremii', 'Precizia Citirii Barometrului'],
                         evidence_card=[3, 3],
-                        values=[[0.8, 0.8, 0.9, 0.4, 0.4, 0.4, 0.1, 0.1, 0.2],  # Da
-                                [0.2, 0.2, 0.1, 0.6, 0.6, 0.6, 0.9, 0.9, 0.8]], # Nu
-                        state_names={'Ploaie': ['Da', 'Nu'],
+                        values=[[0.8, 0.8, 0.9, 0.4, 0.4, 0.4, 0.1, 0.1, 0.2],  # Nu
+                                [0.2, 0.2, 0.1, 0.6, 0.6, 0.6, 0.9, 0.9, 0.8]], # Da
+                        state_names={'Ploaie': ['Nu', 'Da'],
                                      'Starea Vremii': ['Soare', 'Noros', 'Ploios'],
                                      'Precizia Citirii Barometrului': ['Mare', 'Medie', 'Scazuta']})
 
